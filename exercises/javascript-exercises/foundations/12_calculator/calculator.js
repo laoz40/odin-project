3e73a101a9a845796c40af36254c70c561a06b4c
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2026-07-24 04:33:00 +1000

    exercises: Completed calculator

--- exercises/javascript-exercises/foundations/12_calculator/calculator.js
+++ exercises/javascript-exercises/foundations/12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  let sum = 0;
+  numbers.forEach((number) => {
+    sum = sum + number;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  let total = 1;
+  numbers.forEach((number) => {
+    total = total * number;
+  });
+  return total;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (x) {
+  let result = 1;
+  for (let i = 1; i <= x; i++) {
+    result = result * i;
+  }
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +41,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
