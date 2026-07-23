502605a1b09170fcdde049b7f6d5701abeb68c5b
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2026-07-24 04:02:13 +1000

    exercises: Completed sumAll
    Didn't use the sum formula initially because I forgot everything about
    Maths.

--- exercises/javascript-exercises/foundations/09_sumAll/sumAll.js
+++ exercises/javascript-exercises/foundations/09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+    return "ERROR";
+  }
 
+  const min = Math.min(a, b);
+  const max = Math.max(a, b);
+
+	return ((max - min + 1) * (min + max)) / 2;
+
+	// -- Bad O(n) solution
+  // let sum = 0;
+  // for (let i = min; i <= max; i++) {
+  //   sum = sum + i;
+  // }
+  // return sum;
 };
 
 // Do not edit below this line
