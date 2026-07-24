538254cc812b09aa8622798fa3e6c5aa119fd1dc
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2026-07-24 17:36:52 +1000

    rps: Created getComputerChoice function

--- 03-rock-paper-scissors/script.js
+++ 03-rock-paper-scissors/script.js
@@ -1,1 +1,13 @@
-console.log("Hello, world!")
+function getComputerChoice() {
+  const randomInt = Math.floor(Math.random() * 3);
+  switch (randomInt) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+  }
+}
+
+console.log(getComputerChoice())
