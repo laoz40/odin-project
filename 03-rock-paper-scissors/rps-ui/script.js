74cfbca449117a4d38102760e29896e9d7ccd4e5
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2026-07-24 19:23:35 +1000

    odin-project: Add links and files for rps-ui
    Not doing a branch because I have everything in this repo.
    Also redid index.html

---
+++ 03-rock-paper-scissors/rps-ui/script.js
@@ -0,0 +1,70 @@
+function getComputerChoice() {
+  const random = Math.random();
+  if (random < 1 / 3) {
+    return "rock";
+  } else if (random < 2 / 3) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+function getHumanChoice() {
+  return prompt("Rock, Paper, or Scissors?").toLowerCase();
+}
+
+function playRound(humanChoice, computerChoice) {
+  console.log(`You: ${humanChoice}, Bot: ${computerChoice}`);
+
+  if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    return true;
+  } else if (humanChoice === computerChoice) {
+    return "tie";
+  } else {
+    return false;
+  }
+}
+
+function playGame() {
+  console.clear();
+
+  let userScore = 0;
+  let computerScore = 0;
+
+  const rounds = 5;
+  for (let i = 0; i < rounds; i++) {
+    console.log("==================================");
+    console.log(`Round ${i + 1} of ${rounds}`);
+
+    const result = playRound(getHumanChoice(), getComputerChoice());
+
+    switch (result) {
+      case true:
+        userScore++;
+        console.log("ez win!");
+        break;
+      case false:
+        computerScore++;
+        console.log("You suck!");
+        break;
+      case "tie":
+        console.log("Tie!");
+        break;
+    }
+  }
+
+  console.log("==================================");
+  console.log(`Your score: ${userScore}, Computer score: ${computerScore}`);
+
+  if (userScore > computerScore) {
+    console.log("GGEZ");
+  } else if (userScore < computerScore) {
+    console.log("Nice try.");
+  } else {
+    console.log("Tie, you didn't' win.");
+  }
+}
