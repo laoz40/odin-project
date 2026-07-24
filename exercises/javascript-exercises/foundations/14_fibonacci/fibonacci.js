8ae33e4ae85a2ea7fa08e47c9fd85e52e47a16c6
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2026-07-24 15:59:24 +1000

    exercises: Completed fibonacci

--- exercises/javascript-exercises/foundations/14_fibonacci/fibonacci.js
+++ exercises/javascript-exercises/foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(value) {
+	const num = parseInt(value);
 
+	let current = 1;
+	let prev = 1;
+	let next = 0
+
+	if (num === 0) return 0;
+	if (num < 0) return "OOPS";
+	if (num < 2) return 1;
+
+	for (let i = 2; i < num; i++) {
+		next = current + prev;
+		prev = current;
+		current = next;
+	}
+	return current;
 };
 
 // Do not edit below this line
