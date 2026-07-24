6eaf57f71dcdb972fe4abe52d5e4ea0ae067d2ab
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2026-07-24 16:44:17 +1000

    exercises: Completed findTheOldest

--- exercises/javascript-exercises/foundations/16_findTheOldest/findTheOldest.js
+++ exercises/javascript-exercises/foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
 
+	const getAge = (person) => {
+		let age = person.yearOfDeath
+			? person.yearOfDeath - person.yearOfBirth
+			: new Date().getFullYear() - person.yearOfBirth;
+		return age;
+	}
+
+  return people.reduce((oldest, person) => {
+		const oldestAge = getAge(oldest);
+		const personAge = getAge(person);
+
+    return (oldestAge > personAge) ? oldest : person;
+  }, people[0]);
 };
 
 // Do not edit below this line
