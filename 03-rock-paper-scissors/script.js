377ca91c5db64d718dbb33b87e33968d4823543a
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2026-07-24 17:57:30 +1000

    rps: Added logic for Rock Paper Scissors

--- 03-rock-paper-scissors/script.js
+++ 03-rock-paper-scissors/script.js
@@ -1,3 +1,6 @@
+let userScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   const randomInt = Math.floor(Math.random() * 3);
   switch (randomInt) {
@@ -10,4 +13,26 @@ function getComputerChoice() {
   }
 }
 
-console.log(getComputerChoice())
+function getHumanChoice() {
+  return prompt("Rock, Paper, or Scissors?").toLowerCase();
+}
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === "rock" && computerChoice === "scissors") {
+    console.log("You win!");
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    console.log("You win!");
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    console.log("You win!");
+  } else if (humanChoice === computerChoice) {
+    console.log("It's a tie!");
+  } else {
+    console.log("You suck!");
+  }
+  console.log(`Computer chose ${computerChoice}, you chose ${humanChoice}`);
+}
+
+playRound(humanChoice, computerChoice);
